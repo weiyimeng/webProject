79b9756dadba2343c911b7a55eff336d8b776110
weiyimeng <782529847@qq.com>
2022-11-18 17:47:26 +0800

    module--

---
+++ js/first.js
@@ -0,0 +1,12 @@
+class Student {
+    // constructor() {
+    name = "xiaoming"
+        // }
+
+}
+export default Student;
+
+export function def() {
+    console.log("sss222sssss")
+}
+export const hah = "s111ss"
