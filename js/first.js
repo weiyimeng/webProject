a61b29e42e94ded95545d8f2a80343348b188449
weiyimeng <18518635770@163.com>
2022-11-19 10:02:34 +0800

    11

---
+++ js/first.js
@@ -0,0 +1,18 @@
+// var s = "";
+const s = new Set();
+s.add("s").add("d").add("h");
+
+s.forEach((s, b, v) => {
+    console.log(s, b, v)
+})
+s.array.forEach(element => {
+
+});
+
+s.forEach(function() {
+
+}, document)
+
+// "ss".fo(function(s) {
+//     console.log(s);
+// })
